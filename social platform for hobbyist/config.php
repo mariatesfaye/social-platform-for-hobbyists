3cef1f016e23ecfaf50564bbd22caa8bc128e80b
mariatesfaye <120381823+mariatesfaye@users.noreply.github.com>
2024-01-06 12:01:35 +0100

    Update config.php

--- social platform for hobbyist/config.php
+++ social platform for hobbyist/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbHost ='localhost';
-$dbUsername ='root';
-$dbPassword ='';
-$dbDatabase ='tutorial_db';
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbDatabase = 'tutorial_db';
 $conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
 
 ?>
